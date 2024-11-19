56970e28d52be46b35214b3643e4ff5e87edba88
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2024-11-19 16:42:16 +0400

    cookie using refactoring

--- Kudiyarov.StreetFighter6/Extensions/HttpClientExtensions.cs
+++ Kudiyarov.StreetFighter6/Extensions/HttpClientExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Net;
+﻿using Microsoft.Net.Http.Headers;
 
 namespace Kudiyarov.StreetFighter6.Extensions;
 
@@ -12,61 +12,13 @@ public static class HttpClientExtensions
         
         ArgumentNullException.ThrowIfNull(options);
 
-        var handler = GetHttpClientHandler(options.Cookie);
-
         builder.Services
             .AddHttpClient<StreetFighterClient>(client =>
             {
-                var userAgent = options.UserAgent;
-                client.DefaultRequestHeaders.UserAgent.ParseAdd(userAgent);
-            })
-            .ConfigurePrimaryHttpMessageHandler(() =>
-            {
-                var cookie = options.Cookie;
-                return GetHttpClientHandler(cookie);
+                client.DefaultRequestHeaders.Add(HeaderNames.UserAgent, options.UserAgent);
+                client.DefaultRequestHeaders.Add(HeaderNames.Cookie, options.Cookie);
             })
             .AddStandardResilienceHandler();
-    }
-
-    private static HttpClientHandler GetHttpClientHandler(string cookie)
-    {
-        var handler = new HttpClientHandler();
-        var cookies = GetCookies(cookie);
-        handler.CookieContainer.Add(cookies);
-        return handler;
-    }
-
-    private static CookieCollection GetCookies(string cookie)
-    {
-        ArgumentException.ThrowIfNullOrWhiteSpace(cookie);
-        
-        var rawCookies = cookie.Split(';', StringSplitOptions.TrimEntries);
-        var cookies = GetCookies(rawCookies);
-        return cookies;
-    }
-
-    private static CookieCollection GetCookies(string[] rawCookies)
-    {
-        var cookies = new CookieCollection();
-        
-        foreach (var rawCookie in rawCookies)
-        {
-            var cookie = GetCookie(rawCookie);
-            cookies.Add(cookie);
-        }
-
-        return cookies;
-    }
-
-    private static Cookie GetCookie(string rawCookie)
-    {
-        var cookieParts = rawCookie.Split('=');
-
-        var name = cookieParts[0];
-        var value = cookieParts[1];
-
-        var cookie = new Cookie(name, value, "/", "www.streetfighter.com");
-        return cookie;
     }
 }
 
