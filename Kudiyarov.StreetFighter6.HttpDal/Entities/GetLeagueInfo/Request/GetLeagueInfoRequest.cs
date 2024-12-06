6f669520f17059f73cfa49e5dd02e6a9e3a167d2
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2024-12-06 16:13:26 +0400

    GetLeagueInfoRequest

---
+++ Kudiyarov.StreetFighter6.HttpDal/Entities/GetLeagueInfo/Request/GetLeagueInfoRequest.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace Kudiyarov.StreetFighter6.HttpDal.Entities.GetLeagueInfo.Request;
+
+public record GetLeagueInfoRequest
+{
+    [JsonPropertyName("targetShortId")]
+    public required long TargetShortId { get; init; }
+    
+    [JsonPropertyName("targetSeasonId")]
+    public required int TargetSeasonId { get; init; }
+    
+    [JsonPropertyName("locale")]
+    public required string Locale { get; init; }
+    
+    [JsonPropertyName("peak")]
+    public required bool Peak { get; init; }
+}
