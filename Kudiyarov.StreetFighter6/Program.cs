a2afd737d9c912afe5dc49ad82f72718cafbe69c
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2024-12-08 00:49:58 +0400

    top level main

--- Kudiyarov.StreetFighter6/Program.cs
+++ Kudiyarov.StreetFighter6/Program.cs
@@ -1,3 +1,4 @@
+using Kudiyarov.StreetFighter6;
 using Kudiyarov.StreetFighter6.Common.Entities;
 using Kudiyarov.StreetFighter6.Extensions;
 using Kudiyarov.StreetFighter6.Logic;
@@ -5,52 +6,46 @@ using Kudiyarov.StreetFighter6.StyleProviders;
 using Kudiyarov.StreetFighter6.TableWorkers;
 using Spectre.Console;
 
-namespace Kudiyarov.StreetFighter6;
-
-internal static class Program
-{
-    public static async Task Main(string[] args)
-    {
-        var builder = Host.CreateApplicationBuilder(args);
-        var configuration = builder.Configuration.GetRequiredValue<Configuration>("Configuration");
-        var authentication = builder.Configuration.GetRequiredValue<Authentication>("Authentication");
+var builder = Host.CreateApplicationBuilder(args);
+var configuration = builder.Configuration.GetRequiredValue<Configuration>("Configuration");
+var authentication = builder.Configuration.GetRequiredValue<Authentication>("Authentication");
         
-        builder.Logging.ClearProviders();
-        builder.Services.AddScoped<StreetFighterLogic>();
-        builder.Services.AddScoped<InitAction>();
-        builder.Services.AddScoped<UpdateAction>();
-        builder.Services.AddStreetFighterClient(authentication);
-        builder.Services.AddSingleton<StyleProvider<Percentage>, WinRateStyleProvider>();
-        builder.Services.AddSingleton<StyleProvider<LeagueEnum>, LeagueInfoStyleProvider>();
-        builder.Services.AddMemoryCache();
+builder.Logging.ClearProviders();
+builder.Services.AddScoped<StreetFighterLogic>();
+builder.Services.AddScoped<InitAction>();
+builder.Services.AddScoped<UpdateAction>();
+builder.Services.AddStreetFighterClient(authentication);
+builder.Services.AddSingleton<StyleProvider<Percentage>, WinRateStyleProvider>();
+builder.Services.AddSingleton<StyleProvider<LeagueEnum>, LeagueInfoStyleProvider>();
+builder.Services.AddMemoryCache();
 
-        var app = builder.Build();
-        var table = new Table();
-        var request = new GetCharacterInfosRequest(configuration.ProfileId);
+var app = builder.Build();
+var table = new Table();
+var request = new GetCharacterInfosRequest(configuration.ProfileId);
 
-        await AnsiConsole.Live(table)
-            .StartAsync(async ctx =>
-            {
-                await InvokeTableAction<InitAction>(request, ctx, table, app.Services);
-
-                while (true)
-                {
-                    await Task.Delay(configuration.Delay);
-                    await InvokeTableAction<UpdateAction>(request, ctx, table, app.Services);
-                }
-            });
-    }
- 
-    private static async Task InvokeTableAction<TAction>(
-        GetCharacterInfosRequest request,
-        LiveDisplayContext ctx,
-        Table table,
-        IServiceProvider serviceProvider)
-        where TAction : TableAction
+await AnsiConsole.Live(table)
+    .StartAsync(async ctx =>
     {
-        await using var scope = serviceProvider.CreateAsyncScope();
-        var action = scope.ServiceProvider.GetRequiredService<TAction>();
-        await action.Invoke(request, table);
-        ctx.Refresh();
-    }
+        await InvokeTableAction<InitAction>(request, ctx, table, app.Services);
+
+        while (true)
+        {
+            await Task.Delay(configuration.Delay);
+            await InvokeTableAction<UpdateAction>(request, ctx, table, app.Services);
+        }
+    });
+
+return;
+
+static async Task InvokeTableAction<TAction>(
+    GetCharacterInfosRequest request,
+    LiveDisplayContext ctx,
+    Table table,
+    IServiceProvider serviceProvider)
+    where TAction : TableAction
+{
+    await using var scope = serviceProvider.CreateAsyncScope();
+    var action = scope.ServiceProvider.GetRequiredService<TAction>();
+    await action.Invoke(request, table);
+    ctx.Refresh();
 }
