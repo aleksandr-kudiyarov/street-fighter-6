468c24aa307f25ab13650b4274103c2557c765d8
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2024-12-07 21:18:29 +0400

    logic impl

---
+++ Kudiyarov.StreetFighter6.Common/CharacterInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace Kudiyarov.StreetFighter6.Common;
+
+public record CharacterInfo
+{
+    public required int CharacterId { get; init; }
+    public required string CharacterName { get; init; }
+    public required int CharacterSort { get; init; }
+    
+    public required int WinCount { get; init; }
+    public required int BattleCount { get; init; }
+    
+    public required int LeaguePoint { get; init; }
+}
--- Kudiyarov.StreetFighter6/Logic/Implementations/Logic.cs
+++ Kudiyarov.StreetFighter6/Logic/Implementations/Logic.cs
@@ -1,3 +1,4 @@
+using Kudiyarov.StreetFighter6.Common;
 using Kudiyarov.StreetFighter6.HttpDal;
 using Kudiyarov.StreetFighter6.HttpDal.Entities.GetLeagueInfo.Response;
 using Kudiyarov.StreetFighter6.HttpDal.Entities.GetWinRates.Response;
@@ -5,20 +6,32 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace Kudiyarov.StreetFighter6.Logic.Implementations;
 
-public interface ILogic
-{
-    
-}
-
 public class Logic(
     StreetFighterClient client,
     IMemoryCache cache)
 {
+    public async Task<IEnumerable<CharacterInfo>> GetCharacterInfos(CancellationToken cancellationToken = default)
+    {
+        var winRate = await GetWinRate(cancellationToken);
+        var leagueInfo = await GetLeagueInfo(cancellationToken);
+
+        var response = winRate.CharacterWinRate.Join(leagueInfo.CharacterLeagueInfos,
+            left => left.CharacterId,
+            right => right.CharacterId,
+            GetCharacterInfo);
+        
+        return response;
+    }
+
     private async Task<GetWinRateResponse> GetWinRate(CancellationToken cancellationToken = default)
     {
         var winRate = await cache.GetOrCreateAsync(
             "GetWinRate",
-            async entry => await client.GetWinRate(cancellationToken));
+            async entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5);
+                return await client.GetWinRate(cancellationToken);
+            });
         
         ArgumentNullException.ThrowIfNull(winRate);
         return winRate;
@@ -28,9 +41,28 @@ public class Logic(
     {
         var leagueInfo = await cache.GetOrCreateAsync(
             "GetLeagueInfo",
-            async entry => await client.GetLeagueInfo(cancellationToken));
+            async entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5);
+                return await client.GetLeagueInfo(cancellationToken);
+            });
         
         ArgumentNullException.ThrowIfNull(leagueInfo);
         return leagueInfo;
+    }
+
+    private static CharacterInfo GetCharacterInfo(CharacterWinRates winRate, CharacterLeagueInfo leagueInfo)
+    {
+        var result = new CharacterInfo
+        {
+            CharacterId = winRate.CharacterId,
+            CharacterName = winRate.CharacterName,
+            CharacterSort = winRate.CharacterSort,
+            WinCount = winRate.WinCount,
+            BattleCount = winRate.BattleCount,
+            LeaguePoint = leagueInfo.LeagueInfo.LeaguePoint
+        };
+        
+        return result;
     }
 }
